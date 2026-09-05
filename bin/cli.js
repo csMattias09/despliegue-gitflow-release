069dd5424b3fce8e0e874a075189af8d7ea8f846
csMattias09 <22200125@ue.edu.pe>
2026-09-04 23:53:18 -0500

    feat: agrega formulario de login

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 
 const { greet } = require("../src/greet");
+const { login } = require("../src/login");
 
-const name = process.argv[2];
-console.log(greet(name));
+const [comando, ...args] = process.argv.slice(2);
+
+if (comando === "login") {
+  const [username, password] = args;
+  const resultado = login(username, password);
+  console.log(resultado.message);
+  process.exitCode = resultado.ok ? 0 : 1;
+} else {
+  console.log(greet(comando));
+}
